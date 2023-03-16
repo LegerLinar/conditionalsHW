c2c9a51c6ef0ba5a0db673469c5109c76bea8657
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-16 21:17:37 +0300

    Task 1 Mature test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    
+        public static void main(String[] args) {
+            task1();
+            task2();
+        }
+
+        public static void task1 () {
+            System.out.println("Задача 1");
+
+            int boy = 17;
+            int man = 25;
+
+            if(boy >= 18) {
+                System.out.println("Если возраст человека равен " + boy + " он совершеннолетний");
+            } else {
+                System.out.println("Если возраст человека равен " + boy + " он не достиг совершеннолетия, нужно немного подождать");
+            }
+
+            if(man >= 18) {
+                System.out.println("Если возраст человека равен " + man + " он совершеннолетний");
+            } else {
+                System.out.println("Если возраст человека равен " + man + " он не достиг совершеннолетия, нужно немного подождать");
+            }
+
+        }
+
+        public static void task2 () {
+            System.out.println("Задача 2");
+//        ... // Пишем код для задачи 2
+        }
 }
